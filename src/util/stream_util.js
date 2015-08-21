ac643dd1f0ade0f02651db790a9d6675a6bf746c
amobiz <amobiz.tw+github@gmail.com>
2015-08-21 21:45:36 +0800

    rename empty_stream to stream_util, and add chain() method

---
+++ src/util/stream_util.js
@@ -0,0 +1,27 @@
+var inherits = require('util').inherits;
+var Readable = require('stream').Readable;
+
+function EmptyStream() {
+    Readable.call(this);
+}
+
+inherits(EmptyStream, Readable);
+
+EmptyStream.prototype._read = function() {
+    this.emit('end');
+};
+
+function empty() {
+    return new EmptyStream();
+}
+
+function chain(pipe) {
+    var through = require('through2');
+    var duplexer = require('duplexer');
+    var writer = through.obj();
+    var chained = pipe(writer);
+    return duplexer({objectMode: true}, writer, chained);
+}
+
+exports.chain = chain;
+exports.empty = empty;
