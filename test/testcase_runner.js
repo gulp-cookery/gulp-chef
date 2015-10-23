dc686a3aa8097ca2889c8ee52a0247d31922f1b1
amobiz <amobiz.tw+github@gmail.com>
2015-10-23 23:51:26 +0800

    add testcase runner for mocha test

---
+++ test/testcase_runner.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var Sinon = require('sinon');
+var Chai = require('chai');
+var Promised = require("chai-as-promised");
+var expect = Chai.expect;
+Chai.use(Promised);
+
+function test(runner, testCases) {
+    var tests = filter(only) || filter(skip) || testCases; 
+    tests.forEach(function(testCase) {
+        it(testCase.title, function() {
+            if (testCase.debug) {
+                debugger;
+            }
+
+            var to = testCase.async ? 'eventually' : 'to';
+            if (testCase.error) {
+                expect(function() { runner(testCase.value, testCase.options); })[to].throw(testCase.error);
+            } else {
+                expect(runner(testCase.value, testCase.options))[to].deep.equal(testCase.expected);
+            }
+        });
+    });
+
+    function filter(fn) {
+        var tests = testCases.filter(fn);
+        if (tests.length) {
+            return tests;
+        }
+    }
+    
+    function only(testCase) {
+        return testCase.only;
+    }        
+    
+    function skip(testCase) {
+        return !testCase.skip;
+    }
+}
+
+module.exports = test;
