427299972b0870cec88442db3b670d1940d3f3c4
amobiz <amobiz.tw+github@gmail.com>
2015-10-25 01:30:32 +0800

    add FakeGulp

---
+++ test/specs/fake_gulp.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function FakeGulp() {
+	this.taskRegistry = {};
+}
+
+FakeGulp.prototype.task = function(name, runner) {
+	if (typeof name === 'function') {
+		runner = name;
+		name = runner.displayName || runner.name;
+	}
+	if (typeof name === 'string' && typeof runner === 'function') {
+		this.taskRegistry[name] = runner;
+	}
+	return this.taskRegistry[name];
+};
+
+module.exports = FakeGulp;
