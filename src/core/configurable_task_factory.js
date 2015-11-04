844b7442a3efd2a40702bc0cfc9cfd3befce02d8
amobiz <amobiz.tw+github@gmail.com>
2015-11-05 04:18:06 +0800

    introduce ConfigurableTaskFactory()

--- src/core/configurable_task_factory.js
+++ src/core/configurable_task_factory.js
@@ -19,6 +19,41 @@ var CONSTANT = {
 	}
 };
 
+function ConfigurableTaskFactory(stuff, runnerFactory) {
+
+}
+
+ConfigurableTaskFactory.prototype.one = function(prefix, name, rawConfig, parentConfigs) {
+
+};
+
+ConfigurableTaskFactory.prototype.multiple = function(prefix, subTaskConfigs, parentConfigs) {
+
+};
+
+// TODO: make sure config is inherited at config time and injectable at runtime.
+ConfigurableTaskFactory.prototype.create = function(prefix, taskInfo, taskConfig, configurableRunner) {
+	// invoked from stream processor
+	var run = function(gulp, injectConfig, stream, done) {
+		// inject and realize runtime configuration.
+		// TODO: let json-normalizer add defaults.
+		var config = Configuration.realize(taskConfig, injectConfig, configurableRunner.defaults);
+		return configurableRunner(gulp, config, stream, done);
+	};
+	// invoked from gulp
+	var configurableTask = function(done) {
+		return run(this, taskConfig, null, done);
+	};
+	configurableTask.displayName = prefix + taskInfo.name;
+	configurableTask.description = taskInfo.description || configurableRunner.description;
+	configurableTask.visibility = taskInfo.visibility;
+	configurableTask.runtime = taskInfo.runtime;
+	configurableTask.run = run;
+	configurableTask.config = taskConfig;
+	return configurableTask;
+};
+
+
 function getTaskRuntimeInfo(name) {
 	var match;
 
@@ -42,32 +77,10 @@ function isDisabled(task) {
 	return task.visibility === CONSTANT.VISIBILITY.DISABLED;
 }
 
-// TODO: make sure config is inherited at config time and injectable at runtime.
-function create(prefix, taskInfo, taskConfig, configurableRunner) {
-	// invoked from stream processor
-	var run = function(gulp, injectConfig, stream, done) {
-		// inject and realize runtime configuration.
-		// TODO: let json-normalizer add defaults.
-		var config = Configuration.realize(taskConfig, injectConfig, configurableRunner.defaults);
-		return configurableRunner(gulp, config, stream, done);
-	};
-	// invoked from gulp
-	var configurableTask = function(done) {
-		return run(this, taskConfig, null, done);
-	};
-	configurableTask.displayName = prefix + taskInfo.name;
-	configurableTask.description = taskInfo.description || configurableRunner.description;
-	configurableTask.visibility = taskInfo.visibility;
-	configurableTask.runtime = taskInfo.runtime;
-	configurableTask.run = run;
-	configurableTask.config = taskConfig;
-	return configurableTask;
-}
+ConfigurableTaskFactory.CONSTANT = CONSTANT;
+ConfigurableTaskFactory.getTaskRuntimeInfo = getTaskRuntimeInfo;
+ConfigurableTaskFactory.isVisible = isVisible;
+ConfigurableTaskFactory.isDisabled = isDisabled;
+ConfigurableTaskFactory.create = ConfigurableTaskFactory.prototype.create;
 
-module.exports = {
-	CONSTANT: CONSTANT,
-	getTaskRuntimeInfo: getTaskRuntimeInfo,
-	isVisible: isVisible,
-	isDisabled: isDisabled,
-	create: create
-};
+module.exports = ConfigurableTaskFactory;
