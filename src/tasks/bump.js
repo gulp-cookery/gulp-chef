0fd09a2a8bca9c47b6383e4e0b0700fe1956f338
amobiz <amobiz.tw+github@gmail.com>
2015-11-15 20:18:55 +0800

    make bump task use gulp-bump only

--- src/tasks/bump.js
+++ src/tasks/bump.js
@@ -1,11 +1,20 @@
 'use strict';
 
-var message = {
-	type: 'list',
-	name: 'bump',
-	message: 'What type of version bump would you like to do?',
-	choices: ['patch', 'minor', 'major'],
-	"default": 'patch'
+var messages = {
+	version: {
+		type: 'list',
+		name: 'type',
+		message: 'What type of version bump would you like to do?',
+		choices: ['patch', 'minor', 'major'],
+		"default": 'patch'
+	},
+	release: {
+		type: 'list',
+		name: 'release',
+		message: 'What type of release would you like to do?',
+		choices: ['release', 'prerelease', 'beta', 'alpha'],
+		"default": 'release'
+	}
 };
 
 /**
@@ -14,37 +23,45 @@ var message = {
  *
  * Ingredients:
  * gulp-bump
- * gulp-config-sync
  * gulp-prompt
  *
+ * Note:
+ * gulp-bump 可以一次修改多個 .json 檔案，針對每個檔案讀取其 version 資訊，然後各自修改其版本號碼。
+ * 所以可能造成設定不同步的狀況，並不遵循 single source of truth 原則。
  */
 function bumpTask(gulp, config, stream, done) {
 	// lazy loading required modules.
 	var _ = require('lodash'),
 		bump = require('gulp-bump'),
-		configSync = require('gulp-config-sync'),
-		prompt = require('gulp-prompt').prompt;
+		prompt = require('gulp-prompt').prompt,
+		semver = require('semver');
 
-	var options = _.defaultsDeep({}, config.options, defaults.options);
+	var cwd,
+		pkg,
+		newVersion;
 
-	// prompt value is in res.bump
-	prompt(message, function(res) {
-		// note:
-		// gulp-bump 可以一次修改多個 .json 檔案，但是並不遵循 single source of truth 原則。
-		// gulp-bump 針對每個檔案讀取其 version 資訊，然後各自修改其版本號碼。所以可能造成設定不同步的狀況。
-		// 因此，實際使用時，最好只指定 package.json 進行版本修定，然後再利用 gulp-config-sync 之類的 plugin 來進行同步。
-		gulp.src(config.source)
-			.pipe(bump({
-				type: res.bump
-			}))
-			.pipe(gulp.dest(config.base))
-			.pipe(gulp.src(config.target, {
-				base: config.base
-			}))
-			.pipe(configSync(options))
-			.pipe(gulp.dest(config.base))
-			.on('end', done);
-	});
+	cwd = process.cwd();
+	pkg = require(cwd + '/package.json', 'utf8');
+
+	if (config.options.interactive) {
+		// prompt value is in res.bump
+		prompt(messages.version, function(res1) {
+			prompt(messages.release, function(res2) {
+				newVersion = semver.inc(pkg.version, res1.type, res2.release === 'release' ? '' : res2.release);
+				bumpTo(newVersion)
+					.on('end', done);
+			});
+		});
+	} else {
+		newVersion = semver.inc(pkg.version, 'patch');
+		return bumpTo(newVersion);
+	}
+
+	function bumpTo(newVersion) {
+		return gulp.src(config.src.globs)
+			.pipe(bump({ version: newVersion }))
+			.pipe(gulp.dest(config.dest.path));
+	}
 }
 
 bumpTask.schema = {
