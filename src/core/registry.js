4e47683c6cee10b638732e93f73272b73eeed274
amobiz <amobiz.tw+github@gmail.com>
2015-11-18 04:34:47 +0800

    rewrite registry referring undertaker-registry

--- src/core/registry.js
+++ src/core/registry.js
@@ -1,8 +1,20 @@
 "use strict";
 
+/**
+ * @constructor
+ *
+ * Reference:
+ * https://github.com/gulpjs/undertaker-registry/blob/master/index.js
+ *
+ */
 function Registry() {
-	this._tasks = [];
+	this._tasks = {};
 }
+
+Registry.prototype.init = function (gulp) {
+	// NOTE: gulp 4.0 task are called on undefined context. So we need gulp reference here.
+	this.gulp = gulp;
+};
 
 Registry.prototype.get = function (name) {
 	return this._tasks[name];
@@ -12,13 +24,13 @@ Registry.prototype.set = function (name, task) {
 	return (this._tasks[name] = task);
 };
 
-Registry.prototype.init = function (gulp) {
-	// NOTE: gulp 4.0 task are called on undefined context. So we need gulp reference here.
-	this.gulp = gulp;
-};
-
 Registry.prototype.tasks = function () {
-	return this._tasks;
+	var self = this;
+
+	return Object.keys(this._tasks).reduce(function(tasks, name) {
+		tasks[name] = self.get(name);
+		return tasks;
+	}, {});
 };
 
 module.exports = Registry;
