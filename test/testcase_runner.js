21d5461ef07b5207b984ecddcb61df4c2b5ed365
amobiz <amobiz.tw+github@gmail.com>
2015-11-23 04:25:32 +0800

    TestCaseRunner: allow case specific runner

--- test/testcase_runner.js
+++ test/testcase_runner.js
@@ -16,6 +16,7 @@ Chai.use(Promised);
  *       value: 'input value for test',
  *       expected: 'expected output value',
  *       error: 'expected error',
+ *       runner: function(value, options) {},	// specific runner for this case
  *       options: { description: 'options passed to runner for this case' },
  *       async: false,	// is this an async test? i.e. return promise?
  *       only: true,	// run this case only?
@@ -44,10 +45,11 @@ function cases(testCases, runner, options) {
     tests.forEach(function (testCase) {
         it(prefix + testCase.name, function () {
             var to = (testCase.async || options.async) ? 'eventually' : 'to';
+			var run = testCase.runner || runner;
             if (testCase.error) {
-                expect(function () { runner(testCase.value, testCase.options); })[to].throw(testCase.error);
+                expect(function () { run(testCase.value, testCase.options); })[to].throw(testCase.error);
             } else {
-                expect(runner(testCase.value, testCase.options))[to].deep.equal(testCase.expected);
+                expect(run(testCase.value, testCase.options))[to].deep.equal(testCase.expected);
             }
         });
     });
