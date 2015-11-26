db56d3a108714035c2954ecbc25a0d83bb417643
amobiz <amobiz.tw+github@gmail.com>
2015-11-27 02:50:22 +0800

    add todo item

--- src/core/configurable_runner_factory.js
+++ src/core/configurable_runner_factory.js
@@ -36,6 +36,7 @@ var Configuration = require('./configuration'),
 var parallel = require('../flows/parallel');
 
 function hasSubTasks(config) {
+	// TODO: determine should Configuration.sort() treat "task" as "subTaskConfigs".
 	return config.taskInfo.task || _.size(config.subTaskConfigs) > 0;
 }
 
