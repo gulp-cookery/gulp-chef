abdab0d7b55dc9e9b0d2938a50fd290397b6d4e9
amobiz <amobiz.tw+github@gmail.com>
2015-11-28 21:22:45 +0800

    fix unique names

--- src/helpers/unique_names.js
+++ src/helpers/unique_names.js
@@ -1,26 +1,32 @@
 'use strict';
 
 function UniqueNames(prefix) {
-	this.prefix = prefix || 'anonymous';
+	this.prefix = prefix || 'anonymous-';
 	this.names = {};
 	this.names[this.prefix] = 0;
 }
 
-UniqueNames.prototype.valid = function(name) {
-	return (typeof name === 'string' && name.length > 0 && ! (name in this.names));
-}
-
 UniqueNames.prototype.put = function(name) {
-	if (this.valid(name)) {
-		this.names[name] = 0;
+	var names = this.names;
+	if (unique(name)) {
+		names[name] = 0;
+	}
+
+	function unique(name) {
+		return (typeof name === 'string' && name.length > 0 && ! (name in names));
 	}
 };
 
 UniqueNames.prototype.get = function(name) {
-	while (!this.valid(name)) {
-		name = this.prefix + (++this.names[this.prefix]);
+	var names = this.names;
+	return unique(name || this.prefix);
+
+	function unique(name) {
+		if (names[name]++ === 0) {
+			return name;
+		}
+		return name + names[name];
 	}
-	return name;
 };
 
 module.exports = UniqueNames;
