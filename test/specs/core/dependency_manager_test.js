f9c53ed11530b6e5c625990c7893653589949fc3
amobiz <amobiz.tw+github@gmail.com>
2015-12-04 04:21:50 +0800

    DependencyManager: init test

--- test/specs/core/dependency_manager_test.js
+++ test/specs/core/dependency_manager_test.js
@@ -7,6 +7,23 @@ var DependencyManager = require(base + '/src/core/dependency_manager');
 
 describe('core', function () {
 	describe('DependencyManager', function () {
+		var manager;
+
+		beforeEach(function () {
+			manager = new DependencyManager({
+				dependencies: {
+					'lodash': '~3.10.1',
+					'globby': '^2.1.0'
+				},
+				devDependencies: {
+					"chai": "^3.2.0",
+					"chai-as-promised": "^5.1.0",
+					"mocha": "^2.3.4",
+					"semver": "^5.1.0"
+				}
+			});
+		});
+
 		describe('#register()', function () {
 			it('should add newest required version', function () {
 			});
