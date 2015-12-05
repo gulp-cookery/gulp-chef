bccaf1175144a69dcc8896c6233e6636d002dc89
amobiz <amobiz.tw+github@gmail.com>
2015-12-06 02:56:45 +0800

    configure(): add "package.json" dependency management

--- src/configure.js
+++ src/configure.js
@@ -3,18 +3,28 @@ var ConfigurableTaskRunnerFactory = require('./core/configurable_runner_factory'
 	ConfigurableTaskFactory = require('./core/configurable_task_factory'),
 	Configuration = require('./core/configuration'),
 	Registry = require('./core/registry'),
+	DependencyManager = require('./core/dependency_manager'),
+	packages = process.cwd() + '/package.json',
+	store = require(packages),
 	stuff = require('./stuff'),
 	helpRunner = stuff.recipes.lookup('help');
 
 // TODO: resolve too many dependencies problem. (optionalDependencies?). [THOUGHT]: check out bumpTask's bumpTask.requires comments.
 function configure(rawConfigs, options) {
 	var registry = new Registry(),
-		runnerFactory = new ConfigurableTaskRunnerFactory(stuff),
+		manager = new DependencyManager(packages),
+		runnerFactory = new ConfigurableTaskRunnerFactory(stuff, manager),
 		taskFactory = new ConfigurableTaskFactory(stuff, runnerFactory, registry),
 		configs = Configuration.sort({}, rawConfigs, {}, Configuration.SCHEMA_COMMONS);
 
 	Configuration.setOptions(options);
 	taskFactory.multiple('', configs.subTaskConfigs, configs.taskConfig);
+	if (manager.flush()) {
+		manager.save(packages + '.new');
+		console.log('Notice: new dependencies required by used recipes, please run "npm update" to install them.')
+		process.exit(1);
+	}
+
 	registry.set('help', taskFactory.create('', {}, {}, helpRunner));
 
 	// export recipes registry
