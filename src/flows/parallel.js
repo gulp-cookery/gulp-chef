e81a1b6d58107993d8247c95dc0ca479d0be50a6
amobiz <amobiz.tw+github@gmail.com>
2015-12-12 04:43:15 +0800

    parallel, series: remove unnecessary context recreation

--- src/flows/parallel.js
+++ src/flows/parallel.js
@@ -16,16 +16,10 @@ function parallel(done) {
 	var async = require('async'),
 		asyncDone = require('async-done');
 
-	var gulp = this.gulp,
-		config = this.config,
-		tasks = this.tasks;
+	var ctx = this;
 
-	async.map(tasks, function (task, itemDone) {
+	async.map(this.tasks, function (task, itemDone) {
 		asyncDone(function (taskDone) {
-			var ctx = {
-				gulp: gulp,
-				config: config
-			};
 			return task.run.call(ctx, taskDone);
 		}, itemDone);
 	}, done);
--- src/flows/series.js
+++ src/flows/series.js
@@ -10,16 +10,10 @@ function series(done) {
 	var async = require('async'),
 		asyncDone = require('async-done');
 
-	var gulp = this.gulp,
-		config = this.config,
-		tasks = this.tasks;
+	var ctx = this;
 
-	async.mapSeries(tasks, function (task, itemDone) {
+	async.mapSeries(this.tasks, function (task, itemDone) {
 		asyncDone(function (taskDone) {
-			var ctx = {
-				gulp: gulp,
-				config: config
-			};
 			return task.run.call(ctx, taskDone);
 		}, itemDone);
 	}, done);
