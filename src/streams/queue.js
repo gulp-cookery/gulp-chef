b29634db14b4bef78e8efce43ff4966532cba9b2
amobiz <amobiz.tw+github@gmail.com>
2015-12-15 00:22:27 +0800

    queue: use stream-helper

--- src/streams/queue.js
+++ src/streams/queue.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var helper = require('./stream-helper')('queue');
+
 /*jshint node: true */
 /*global process*/
 
@@ -20,18 +22,11 @@
 function queue() {
 	// lazy loading required modules.
 	var StreamQueue = require('streamqueue'),
-		PluginError = require('gulp-util').PluginError;
+		helper = require('./stream-helper');
 
 	var context = this,
-		tasks = context.tasks;
-
-	if (context.upstream) {
-		throw new PluginError('queue', 'queue stream-processor do not accept up-stream');
-	}
-
-	if (tasks.length === 0) {
-		throw new PluginError('queue', 'no sub task specified');
-	}
+		tasks = context.tasks,
+		runTask = helper(context);
 
 	if (tasks.length === 1) {
 		return runTask(tasks[0]);
@@ -42,21 +37,6 @@ function queue() {
 			objectMode: true
 		});
 	return streamQueue.done.apply(streamQueue, streams);
-
-	function runTask(task) {
-		var stream = task.run.call(context, done);
-		if (!isStream(stream)) {
-			// TODO: Do not throw errors inside a stream. According to the [Guidelines](https://github.com/gulpjs/gulp/blob/4.0/docs/writing-a-plugin/guidelines.md)
-			throw new PluginError('queue', 'sub task must return a stream');
-		}
-		return stream;
-
-		function done() {}
-	}
-
-	function isStream(target) {
-		return (typeof target.pipe === 'function');
-	}
 }
 
 queue.expose = [];
