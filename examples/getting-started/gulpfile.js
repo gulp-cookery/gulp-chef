2f0dafdaca6d986180a1ec9422a4bfa5b7081e93
amobiz <amobiz.tw+github@gmail.com>
2016-01-03 15:55:07 +0800

    examples: add getting-started

---
+++ examples/getting-started/gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var configure = require('gulp-ccr');
+
+var recipes = configure({
+	clean: function (done) {
+		console.log('hello clean');
+		done();
+	},
+	markups: function (done) {
+		console.log('hello markup');
+		done();
+	},
+	scripts: function (done) {
+		console.log('hello scripts');
+		done();
+	},
+	styles: function (done) {
+		console.log('hello styles');
+		done();
+	},
+	default: ['clean', { parallel: ['scripts', 'styles', 'markups'] }]
+});
+
+gulp.registry(recipes);
