d48682edf3a45320fc0a706c220de3c1b514cdf3
amobiz <amobiz.tw+github@gmail.com>
2016-01-09 02:05:09 +0800

    examples: add webapp-seed

---
+++ examples/webapp-seed/gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp = require('gulp');
+var chef = require('gulp-chef');
+
+var meal = chef({
+  src: 'src',
+  dest: 'dist',
+  clean: {},
+  make: {
+    markups: {
+      src: 'index.html',
+      task: function (done) {
+        done();
+      }
+    },
+    scripts: {
+      src: 'scripts/**/*.js',
+      task: function (done) {
+        done();
+      }
+    },
+    styles: {
+      src: 'styles/**/*.css',
+      task: function (done) {
+        done();
+      }
+    },
+    images: {
+      src: 'images/**/*.*',
+      recipe: 'copy'
+    },
+    assets: {
+      src: '**/*.txt',
+      recipe: 'copy'
+    }
+  },
+  build: ['clean', 'make'],
+  default: 'build'
+});
+
+gulp.registry(meal);
