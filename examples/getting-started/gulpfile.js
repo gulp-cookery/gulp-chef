5a3ec2c2b3d3bcdce89b292519273d87c473864f
amobiz <amobiz.tw+github@gmail.com>
2016-01-11 22:52:39 +0800

    examples/getting-started: becomes an interactive course

--- examples/getting-started/gulpfile.js
+++ examples/getting-started/gulpfile.js
@@ -3,24 +3,90 @@
 var gulp = require('gulp');
 var chef = require('gulp-chef');
 
-var meal = chef({
-  clean: function (done) {
-    console.log('hello clean');
-    done();
-  },
-  markups: function (done) {
-    console.log('hello markup');
-    done();
-  },
-  scripts: function (done) {
-    console.log('hello scripts');
-    done();
-  },
-  styles: function (done) {
-    console.log('hello styles');
-    done();
-  },
-  default: ['clean', { parallel: ['scripts', 'styles', 'markups'] }]
-});
+var prepare = function (courses) {
+  return each(Object.assign(courses, { default: 'intro' }));
 
+  function each(items) {
+    return Object.keys(items).reduce(function (ret, name) {
+      var item, task, msg;
+
+      item = items[name];
+      if (item.msg) {
+        msg = print(item.msg);
+        delete item.msg;
+        item.intro = msg;
+        task = each(item);
+      } else {
+        task = print(item);
+      }
+      ret[name] = task;
+      return ret;
+    }, {});
+  }
+
+  function print(messages) {
+    return function (done) {
+      console.log(Array.isArray(messages) ? messages.join('\n') : messages);
+      done();
+    };
+  }
+};
+
+var COURSES = {
+  intro: [
+    'Hello Chef!',
+    'First time running gulp-chef interactive course?',
+    'Run `gulp --tasks` for available tasks.',
+    'And then run `gulp  <task-name>` for whatever you would like to know about.'
+  ],
+  configuration: {
+    msg: [
+      'TODO: intro about the relationship between recipe and task name.',
+      ''
+    ],
+    recipe: [
+    ],
+    glob: [
+      'TODO: print transformed configuration for all use case.',
+      ''
+    ],
+    path: [
+      'TODO: print transformed configuration for all use case.',
+      ''
+    ],
+    casscading: [
+      'TODO: show how the parent configuration inherited by child.',
+      ''
+    ],
+    _task_: [
+      '',
+      ''
+    ]
+  },
+  'build-in-recipes': [
+    '',
+    ''
+  ],
+  'writing-recipe': [
+    '',
+    ''
+  ],
+  api: [
+    '',
+    ''
+  ],
+  keywords: [
+    '',
+    ''
+  ],
+  cli: [
+    '',
+    ''
+  ]
+};
+
+var recipes, meal;
+
+recipes = prepare(COURSES);
+meal = chef(recipes, { exposeWithPrefix: 'always' });
 gulp.registry(meal);
