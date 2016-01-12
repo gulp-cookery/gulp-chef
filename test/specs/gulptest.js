413c7450eb6c0d59a17f77cb50ba538742eee301
amobiz <amobiz.tw+github@gmail.com>
2016-01-12 08:13:41 +0800

    test/gulptest: add gulp.js cheatsheet samples

---
+++ test/specs/gulptest.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var gulp = require('gulp');
+var chef = require('../../');
+
+var configs = {
+	src: 'test/_fiexures/',
+	dest: 'dist/',
+	'async-streams': {
+		src: 'css/',
+		dest: 'css/',
+		merge: {
+			less: {
+				src: 'first.less',
+				plugin: 'gulp-less'
+			},
+			autoprefixer: {
+				src: 'second.css',
+				plugin: 'gulp-autoprefixer'
+			}
+		}
+	},
+	'serial-join': {
+		src: 'css/',
+		dest: 'css/',
+		pipe: {
+			queue: {
+				less: {
+					src: 'first.less',
+					plugin: 'gulp-less'
+				},
+				pipe: {
+					src: 'second.css',
+					cssimport: {
+						plugin: 'cssimport'
+					},
+					autoprefixer: {
+						plugin: 'gulp-autoprefixer',
+						options: 'last 2 versions'
+					}
+				}
+			},
+			concat: {
+				file: 'app.css',
+				minify: true,
+				spit: true
+			}
+		}
+	},
+	'stream-array': {
+		src: 'template/',
+		dest: 'html/',
+		each: {
+			values: [{
+				name: 'apple',
+				title: 'Apple Cake',
+				price: '25'
+			}, {
+				name: 'orange',
+				title: 'Orange Cookie',
+				price: '18'
+			}],
+			pipe: {
+				consolidate: {
+					src: 'a.js',
+					plugin: 'consolidate',
+					options: {
+						title: '{{title}}',
+						price: '{{price}}'
+					}
+				},
+				rename: {
+					plugin: 'rename',
+					options: {
+						basename: '{{name}}',
+						extname: '.html'
+					}
+				},
+				spit: {
+				}
+			}
+		}
+	},
+	default: 'stream'
+};
+
+var meal = chef(configs);
+
+gulp.registry(meal);
