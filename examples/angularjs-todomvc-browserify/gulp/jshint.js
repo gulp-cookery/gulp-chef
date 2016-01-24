bd73126004e2f962d5f69c4f8b51ddabe99fcf8c
amobiz <amobiz.tw+github@gmail.com>
2016-01-24 22:54:24 +0800

    split example projects

--- examples/angularjs-todomvc-browserify/gulp/jshint.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function () {
-  var jshint = require('gulp-jshint');
-
-  return this.gulp.src(this.config.src.globs)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-};
--- examples/webapp-seed/src/scripts/main.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-(function () {
-  console.log('Hello Chef!');
-})();
